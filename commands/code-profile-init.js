9746a270c208ebbb92b27065be502c822f69a321
Robson Mendonça <rob.ams@gmail.com>
2023-04-04 23:47:46 -0300

    add new command code-profile-init

--- commands/code-profile-init.js
+++ commands/code-profile-init.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("code-profile-init")
-    .setDescription("Visual Code versão online!"),
+    .setDescription("Visual Code Profile com apenas extensões necessaria para o inicio com HTML, CSS e Javascript!"),
     async execute(interaction){
         await interaction.reply("https://github.com/robsonamendonca/GuiaDevJrBot/blob/main/perfil/README.md")
     }
